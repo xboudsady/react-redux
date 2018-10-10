e557b8aab91e99c54ff8f63242932dc9e03ef9b7
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-10-09 21:36:33 -0700

    Created state, and return data to the browser

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,16 +3,24 @@ import Ninjas from './Ninjas';
 
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-            <h1>My first React app!</h1>
-            <p>Welcome :)</p>
-            <Ninjas name="Ryu" age="25" belt="black" />
-            <Ninjas name="Yoshi" age="30" belt="green" />
-        </div>
-    );
-  }
+    state = {
+        ninjas: [
+            { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+            { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+            { name: 'Crystal', age: 25, belt: 'pink', id: 3 },
+        ]
+        
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>My first React app!</h1>
+                <p>Welcome :)</p>
+                <Ninjas ninjas={ this.state.ninjas } />
+            </div>
+        );
+    }
 }
 
 export default App;
