ed6b0493f28100b93adc8cd7d54007f6774d1cfe
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-10-09 21:46:46 -0700

    Converted class base component to functional component

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 
-class Ninjas extends Component {
-    render() {
-        // console.log(this.props);
-        const { ninjas } = this.props
-
-        const ninjaList = ninjas.map(ninja => {
-            return (
-                <div className="ninja" key={ ninja.id }>
-                    <div>Name: { ninja.name }</div>
-                    <div>Age: { ninja.age }</div>
-                    <div>Belt: { ninja.belt }</div>
-                </div>
-            )
-        });
-
-        return(
-            <div className="jinja-list">
-                { ninjaList }
+// This is a UI based component, so we can use a function component, we're not storing state
+const Ninjas = ({ninjas}) => {
+    const ninjaList = ninjas.map(ninja => {
+        return (
+            <div className="ninja" key={ ninja.id }>
+                <div>Name: { ninja.name }</div>
+                <div>Age: { ninja.age }</div>
+                <div>Belt: { ninja.belt }</div>
             </div>
         )
-    }
+    });
+
+    return(
+        <div className="ninja-list">
+            { ninjaList }
+        </div>
+    )
+    
 }
 
 export default Ninjas;
