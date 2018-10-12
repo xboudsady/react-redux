1dea46de9b62b472f8f3c851ad63c8d4b71c2a27
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-10-11 21:20:56 -0700

    Added delete function

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,20 +1,7 @@
 import React, { Component } from 'react';
 
 // This is a UI based component, so we can use a function component, we're not storing state
-const Ninjas = ({ninjas}) => {
-    // const ninjaList = ninjas.map(ninja => {
-    //     if (ninja.age > 20) {
-    //         return (
-    //             <div className="ninja" key={ninja.id}>
-    //                 <div>Name: {ninja.name}</div>
-    //                 <div>Age: {ninja.age}</div>
-    //                 <div>Belt: {ninja.belt}</div>
-    //             </div>
-    //         )
-    //     } else {
-    //         return null
-    //     }
-    // });
+const Ninjas = ({ninjas, deleteNinja}) => {
 
     const ninjaList = ninjas.map(ninja => {
         return ninja.age > 20 ? (
@@ -22,6 +9,7 @@ const Ninjas = ({ninjas}) => {
                 <div>Name: {ninja.name}</div>
                 <div>Age: {ninja.age}</div>
                 <div>Belt: {ninja.belt}</div>
+                <button onClick={ () => { deleteNinja(ninja.id)} } >Delete ninja</button>
             </div>
         ) : null
     })
