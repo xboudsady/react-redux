019753d8b228b2af70631953434f3bd796b79b17
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-10-11 20:55:56 -0700

    Add form submission to render new array of ninjas

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -12,13 +12,21 @@ class App extends Component {
         
     }
 
+    addNinja = (ninja) => {
+        ninja.id = Math.random();
+        let ninjas = [...this.state.ninjas, ninja]
+        this.setState({
+            ninjas: ninjas
+        })
+    }
+
     render() {
         return (
             <div className="App">
                 <h1>My first React app!</h1>
                 <p>Welcome :)</p>
                 <Ninjas ninjas={ this.state.ninjas } />
-                <AddNinja />
+                <AddNinja addNinja={ this.addNinja } />
             </div>
         );
     }
