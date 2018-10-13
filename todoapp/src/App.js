a4f9b8b3e236e8d3a53987a3623c7ded28e9b7cf
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-10-12 19:18:22 -0700

    Created todo app to display state data, and click function to remove item on list

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        todos: [
+            {id: 1, content: 'buy some milk'},
+            {id: 2, content: 'play mario kart'}
+        ]
+    }
+
+    deleteTodo = (id) => {
+        const todos = this.state.todos.filter(todo => {
+            return todo.id !== id
+        });
+        
+        this.setState({
+            todos
+        })
+    }
+
+
+    render() {
+        return (
+            <div className="todo-app container">
+                <h1 className="center blue-text">Todo's</h1>
+                <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}  />
+            </div>
+        );
+    }
 }
 
 export default App;
