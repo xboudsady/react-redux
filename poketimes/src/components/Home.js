5dc071c6507009286a0e709fa0e2009e8504e932
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-10-18 20:21:06 -0700

    Loaded axios to pull data from JSON placeholder on the home page

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = () => {
-    return (
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt ratione necessitatibus aut doloribus tenetur consequatur alias asperiores voluptas iusto ipsam eius nisi, quod corrupti eaque esse, officia nostrum error pariatur.</p>
-        </div>
-    )
+class Home  extends Component {
+    state = {
+        posts: []
+    }
+    
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                console.log(res);
+                this.setState({
+                    posts: res.data.slice(0, 10)
+                })
+            })
+    }
+    
+    render() {
+        const { posts } = this.state;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className="post card" key={post.id}>
+                        <div className="card-content">
+                            <span className="card-title">{post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="center">No posts yet</div>
+        )
+
+        return (
+            <div className="container">
+                <h4 className="center">Home</h4>
+                {postList}
+            </div>
+        )
+    }
+    
 }
 
 export default Home;
---
+++ poketimes/src/components/Post.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h4>route parameter</h4>
+            </div>
+        )
+    }
+}
+
+export default Post
