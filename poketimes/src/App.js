d0dd8b476642b04dd8469100907d9fd655cdf1bb
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-10-18 21:13:51 -0700

    Added Swtich component for correct react routing

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
@@ -8,17 +8,17 @@ import Post from './components/Post';
 
 class App extends Component {
   render() {
-    return (
-      <BrowserRouter>
+    return <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route exact path='/' component={Home} />
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
-          <Route path='/:post_id' component={Post} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/:post_id" component={Post} />
+          </Switch>
         </div>
-      </BrowserRouter>
-    );
+      </BrowserRouter>;
   }
 }
 
