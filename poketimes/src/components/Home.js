b4427f8fc8e28f2d7f1898ee66bc28fc23b5bd74
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-10-18 21:09:31 -0700

    Added single page post

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Home  extends Component {
     state = {
@@ -20,14 +21,16 @@ class Home  extends Component {
         const { posts } = this.state;
         const postList = posts.length ? (
             posts.map(post => {
-                return (
-                    <div className="post card" key={post.id}>
-                        <div className="card-content">
-                            <span className="card-title">{post.title}</span>
-                            <p>{post.body}</p>
-                        </div>
+                return <div className="post card" key={post.id}>
+                    <div className="card-content">
+                        <Link to={'/' + post.id} >
+                            <span className="card-title">
+                            {post.title}
+                            </span>
+                        </Link>
+                      <p>{post.body}</p>
                     </div>
-                )
+                  </div>;
             })
         ) : (
             <div className="center">No posts yet</div>
